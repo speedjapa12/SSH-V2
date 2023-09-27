1a3020a1ece26e77117c1be712f3d94eb99a018c
speedjapa12 <145918086+speedjapa12@users.noreply.github.com>
2023-09-27 08:55:56 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,9 +30,19 @@ dias="2"
 servermessage="<h3><font color='green'>
 ▬▬▬▬▬▬✿𝙒𝙀𝙇𝘾𝙊𝙈𝙀✿▬▬▬▬▬▬▬
 </font></h3>
-<h3><font color='cyan'>
- 🅖🅒🅟🇯🇵ⒿⒶⓅⒶ🇯🇵
-✿𝐏𝐑𝐄𝐌𝐈𝐔𝐌✿𝐒𝐄𝐑𝐕𝐄𝐑✿𝐅𝐑𝐄𝐄✿ 
+<h3><font color='cyan'> 
+🅖🅒🅟🇸🇬ⓈⒾⓃⒼⒶⓅⓄⓇⒺ🇸🇬
+</font></h3>
+<h3><font color='red'>
+𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙈𝙔 𝘾𝙃𝘼𝙉𝙉𝙀𝙇
+https://t.me/tytechnology
+</font></h3>
+<h3><font color='blue'>
+𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙈𝙔 𝘾𝙃𝘼𝙉𝙉𝙀𝙇
+https://t.me/GCPFREE510
+</font></h3>
+<h3><font color='magenta'>
+✿𝐏𝐑𝐄𝐌𝐈𝐔𝐌✿𝐒𝐄𝐑𝐕𝐄𝐑✿𝐅𝐑𝐄𝐄✿
 </font></h3>
 <h3><font color='green'>
 ▬▬▬▬▬▬✿𝙒𝙀𝙇𝘾𝙊𝙈𝙀✿▬▬▬▬▬▬▬
